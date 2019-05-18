15104b8fbf3542cfbc6a44d20db1bf0b3bda4354
john <john@terratechgame.com>
2019-05-18 14:29:32 +0100

    input test scene

---
+++ Assets/Scripts/PowerBar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerBar : MonoBehaviour
+{
+    public float level;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 scale = transform.localScale;
+        scale.x = level;
+        transform.localScale = scale;
+    }
+}
