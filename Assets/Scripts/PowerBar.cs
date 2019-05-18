61dca755c7a49d63794076e16639aaf0ff4a09d9
john <john@terratechgame.com>
2019-05-18 15:49:59 +0100

    Input test scene

--- Assets/Scripts/PowerBar.cs
+++ Assets/Scripts/PowerBar.cs
@@ -1,22 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class PowerBar : MonoBehaviour
 {
-    public float level;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    [SerializeField]
+    InputFreqSystem m_InputFreq;
+    
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         Vector3 scale = transform.localScale;
-        scale.x = level;
-        transform.localScale = scale;
+        scale.x = m_InputFreq.m_PowerOutput;
+        transform.localScale = scale; 
     }
 }
