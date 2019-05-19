e3c0bac0b9a40ca768a7e1e92ea024c8b27c0da6
john <john@terratechgame.com>
2019-05-19 13:58:32 +0100

    Added full progression

--- Assets/Scripts/Progression.cs
+++ Assets/Scripts/Progression.cs
@@ -4,12 +4,23 @@ using UnityEngine;
 
 public class Progression : MonoBehaviour
 {
+    [SerializeField]
+    Transform m_RestartUI;
+
+    [SerializeField]
+    Rigidbody2D m_BoulderRBody;
+
     public static Progression s_Inst;
 
+    Renderer m_BoulderRenderer;
+    Pusher m_BoulderPusher;
+
     public State m_State;
+    float m_Delay;
 
     public enum State
     {
+        GracePeriod,
         Playing,
         GameOver
     }
@@ -27,12 +38,65 @@ public class Progression : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        m_State = State.Playing;
+        if (m_BoulderRBody)
+        {
+            m_BoulderRenderer = m_BoulderRBody.GetComponent<Renderer>();
+            m_BoulderPusher = m_BoulderRBody.GetComponent<Pusher>();
+        }
+
+        m_State = State.GracePeriod;
+        m_Delay = 2f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // todo
+        // update delay
+        if (m_Delay > 0)
+        {
+            m_Delay -= Time.deltaTime;
+        }
+
+        switch (m_State)
+        {
+            case State.GracePeriod:
+                bool enablePusher;
+                if (m_Delay <= 0)
+                {
+                    m_State = State.Playing;
+                    enablePusher = true;
+                }
+                else
+                {
+                    enablePusher = false;
+                }
+                if (m_BoulderPusher)
+                {
+                    m_BoulderPusher.enabled = enablePusher;
+                }                
+                break;
+
+            case State.Playing:
+                if (m_BoulderRenderer)
+                {
+                    if (!m_BoulderRenderer.isVisible)
+                    {
+                        m_State = State.GameOver;
+
+                        if (m_RestartUI)
+                        {
+                            m_RestartUI.gameObject.SetActive(true);
+                        }
+                    }
+                }
+                break;
+
+            case State.GameOver:
+                if (m_BoulderRBody)
+                {
+                    m_BoulderRBody.simulated = false;
+                }
+                break;
+        }
     }
 }
