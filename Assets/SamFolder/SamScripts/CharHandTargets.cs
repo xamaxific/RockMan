65f172739db58b07112ccd3353284e0497d7e0aa
Sam Wong <xamaxific@gmail.com>
2019-05-19 13:17:03 +0100

    updated UI for test level.

---
+++ Assets/SamFolder/SamScripts/CharHandTargets.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharHandTargets : MonoBehaviour
+{
+    [Header( "Target" )]
+    [SerializeField] private Transform m_objectToattach;
+    [SerializeField] private Vector3 m_leftHandTargetOffset;
+    [SerializeField] private Vector3 m_rightHandTargetOffset;
+    [Header("Controls")]
+    [SerializeField] private Transform m_leftHandCtrl;
+    [SerializeField] private Transform m_rightHandCtrl;
+
+
+
+    private void Update () {
+        SetCtrlToTarget();
+    }
+
+    public void SetCtrlToTarget () {
+        Vector3 lHand = m_objectToattach.position + m_leftHandTargetOffset;
+        Vector3 rHand = m_objectToattach.position + m_rightHandTargetOffset;
+        m_leftHandCtrl.position = lHand;
+        m_rightHandCtrl.position = rHand;
+    }
+}
---
+++ Assets/SamFolder/SamScripts/SamPowerBar.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class SamPowerBar : MonoBehaviour
+{
+    [SerializeField] private Slider m_slider;
+
+    void LateUpdate () {
+        m_slider.value = InputFreqSystem.s_Inst.m_PowerOutput;
+    }
+}
